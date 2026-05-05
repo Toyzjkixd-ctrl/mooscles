9634e7b3f06443d130231f898331132e2a1031b2
Toyzjkixd-ctrl <nat.roy86360@gmail.com>
2026-05-05 20:15:05 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ── SERVICE WORKER — Network First ───────────────────────────────────────────
-const CACHE = 'mooscles-v1.2.1';
+const CACHE = 'mooscles-v1.2.2';
 
 // Fichiers à pré-cacher au premier install
 const PRECACHE = [
